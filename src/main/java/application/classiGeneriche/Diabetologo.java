47258967e833647fd474828cc98ba006c77faab8
Reyunityone <94675908+Reyunityone@users.noreply.github.com>
2026-08-20 22:11:30 +0200

    Aggiunta data di fine, vari refactor suggeriti da intellij (#10)

--- src/main/java/application/classiGeneriche/Diabetologo.java
+++ src/main/java/application/classiGeneriche/Diabetologo.java
@@ -4,8 +4,8 @@ public final class Diabetologo extends User{
     public Diabetologo(String username, String codiceFiscale, String nome, String cognome, String email){
         super(username, codiceFiscale, nome, cognome, email);
     }
-
     public Diabetologo(){
         super("mariorossi1", "MMMMMM", "Mario", "Rossi", "rossimario@ulss9.it");
     }
+
 }
